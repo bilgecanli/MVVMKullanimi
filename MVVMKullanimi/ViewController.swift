183aaea558f3a28db43d1039277a0cdf51b37ab6
mehmet <mehmetsaidcanli@gmail.com>
2024-01-21 15:12:37 +0300

    Initial Commit

---
+++ MVVMKullanimi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMKullanimi
+//
+//  Created by Mehmet Said Canlı on 21.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
